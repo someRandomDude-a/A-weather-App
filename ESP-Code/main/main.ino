2746be292935efaa23cb0bfcde19b6896b847742
someRandomDude-a <abhyuditac+github@gmail.com>
2025-09-21 13:46:24 +0530

    Update main.ino

--- ESP-Code/main/main.ino
+++ ESP-Code/main/main.ino
@@ -1,47 +1,58 @@
+/*
+ * This ESP32 code is created by esp32io.com
+ *
+ * This ESP32 code is released in the public domain
+ *
+ * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-http-request
+ */
+
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-const char WIFI_SSID[] = "why are you looking at this";         // CHANGE IT         // CHANGE IT
-const char WIFI_PASSWORD[] = "STOP LOOKING BRUH"; // CHANGE IT
+const char WIFI_SSID[] = "why are you looking at this";
+const char WIFI_PASSWORD[] = "STOP LOOKING BRUH"; 
 
-String HOST_NAME   = "http://YOUR_DOMAIN.com"; // looking for it
-String HOST_NAME   = "http://YOUR_DOMAIN.com"; // idk make this @gaurav
+String HOST_NAME   = "http://YOUR_DOMAIN.com"; // CHANGE IT @gaurav
+//String PATH_NAME   = "/products/arduino.php"; 
+String queryString = "temperature=26&humidity=70";
+
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
-  
+
   HTTPClient http;
 
-  http.begin(HOST_NAME + PATH_NAME); //HTTP
-  int httpCode = http.GET();
+  http.begin(HOST_NAME + PATH_NAME);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  int httpCode = http.POST(queryString);
 
   // httpCode will be negative on error
-  if(httpCode > 0) {
+  if (httpCode > 0) {
     // file found at server
-    if(httpCode == HTTP_CODE_OK) {
+    if (httpCode == HTTP_CODE_OK) {
       String payload = http.getString();
       Serial.println(payload);
     } else {
       // HTTP header has been send and Server response header has been handled
-      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+      Serial.printf("[HTTP] POST... code: %d\n", httpCode);
     }
   } else {
-    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
   }
 
   http.end();
 }
 
 void loop() {
-
 }
